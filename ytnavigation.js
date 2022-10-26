dc411d041ee83af180bab5ebaa5100c72541c247
Amery <47970371+popsicle97@users.noreply.github.com>
2022-10-27 02:48:08 +0800

    Update 0.1 Added Navigation History

--- ytnavigation.js
+++ ytnavigation.js
@@ -1,5 +1,21 @@
+const YOUTUBE_URL = `${window.location.protocol}//${window.location.hostname}/`;
+
 const contentCards = document.querySelectorAll("#content");
 const searchInput = document.getElementById("search-input");
+
+function keyboardListener() {
+  console.log("Check pathname", window.location.protocol);
+  document.addEventListener("keypress", (ev) => {
+    const inputKey = ev.key;
+    switch (inputKey) {
+      case "h":
+        window.location.href = `${YOUTUBE_URL}feed/history`;
+        break;
+      default:
+        break;
+    }
+  });
+}
 
 contentCards.forEach((contentCard) => {
   contentCard.style.border = "1px solid red";
@@ -8,3 +24,5 @@ contentCards.forEach((contentCard) => {
 // searchInput.style.border = "1px solid red";
 
 console.log("Check contentCard", contentCards);
+
+keyboardListener();
